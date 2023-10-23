3d629cd7bca175b384694cc3a17180bf73c8f7a9
alexaig12 <alexa.guerrero@maine.edu>
2023-10-22 22:12:12 -0400

    components

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -2,25 +2,30 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export const COLORS = ["red", "blue", "green"];
-const DEFAULT_COLOR_INDEX = 0;
+//const DEFAULT_COLOR_INDEX = 0;
 
-function ChangeColor(): JSX.Element {
-    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
-    return (
-        <Button onClick={() => setColorIndex((1 + colorIndex) % COLORS.length)}>
-            Next Color
-        </Button>
-    );
+function ChangeColor({
+    colorIndex,
+    setColorIndex
+}: {
+    colorIndex: number;
+    setColorIndex: (newIndex: number) => void;
+}) {
+    const colorChange = () => {
+        setColorIndex((1 + colorIndex) % COLORS.length);
+    };
+
+    return <Button onClick={colorChange}>Next Color</Button>;
 }
 
-function ColorPreview(): JSX.Element {
+function ColorPreview({ colorIndex }: { colorIndex: number }): JSX.Element {
     return (
         <div
             data-testid="colored-box"
             style={{
                 width: "50px",
                 height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
+                backgroundColor: COLORS[colorIndex],
                 display: "inline-block",
                 verticalAlign: "bottom",
                 marginLeft: "5px"
@@ -30,14 +35,53 @@ function ColorPreview(): JSX.Element {
 }
 
 export function ColoredBox(): JSX.Element {
+    const [colorIndex, setColorIndex] = useState<number>(0);
     return (
         <div>
             <h3>Colored Box</h3>
-            <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
+            <span>The current color is: {COLORS[colorIndex]}</span>
             <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
+                <ChangeColor
+                    colorIndex={colorIndex}
+                    setColorIndex={setColorIndex}
+                ></ChangeColor>
+                <ColorPreview colorIndex={colorIndex} />
             </div>
         </div>
     );
+}
+
+{
+    /* this also works, how? just narrows into one function
+    and enter more the details into the return
+    
+export function ColoredBox(): JSX.Element {
+    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
+
+    const changeColor = () => {
+        setColorIndex((colorIndex + 1) % COLORS.length);
+    };
+
+    return (
+        <div>
+            <h3>Colored Box</h3>
+            <span>The current color is: {COLORS[colorIndex]}</span>
+            <div>
+                <Button onClick={changeColor}>Next Color</Button>
+                <div
+                    data-testid="colored-box"
+                    style={{
+                        width: "50px",
+                        height: "50px",
+                        backgroundColor: COLORS[colorIndex],
+                        display: "inline-block",
+                        verticalAlign: "bottom",
+                        marginLeft: "5px"
+                    }}
+                ></div>
+            </div>
+        </div>
+    );
+}
+*/
 }
--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,24 +1,30 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
+//import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): JSX.Element {
-    return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
+/*function Doubler(): JSX.Element {
+    return 
 }
+ these functions are removed becuase it is extract and not needed...
+ when export function NAME(): JSX.Element is already present
 
+ additionally, a file DoubleHalfState.tsx was also deleted...
+ because useState cannot be outside of a component function
+ 
 function Halver(): JSX.Element {
-    return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
-}
+    return 
+}*/
 
 export function DoubleHalf(): JSX.Element {
+    const [dhValue, setDhValue] = useState<number>(10);
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
+            <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
         </div>
     );
 }
