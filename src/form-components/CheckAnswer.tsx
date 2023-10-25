74c55ca902e53f6d3020ceb4073e7a496e5a52f0
alexaig12 <alexa.guerrero@maine.edu>
2023-10-25 12:13:40 -0400

    solved-form-component

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -5,9 +5,30 @@ export function CheckAnswer({
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    //initialize the state to an empty string
+    const [userAnswer, setUserAnswer] = useState("");
+
+    const handleUserAnwerChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setUserAnswer(event.target.value);
+    };
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <input
+                type="text"
+                value={userAnswer}
+                onChange={handleUserAnwerChange}
+            />
+            <div>
+                {userAnswer === expectedAnswer ? (
+                    <span>✔️</span> //Display checkmark if the answers match
+                ) : (
+                    <span>❌</span> //Otherwise display a cross
+                )}
+            </div>
         </div>
     );
 }
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,51 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [editMode, setEditMode] = useState(false);
+    const [userName, setUsername] = useState("Your Name");
+    const [isStudent, setIsStudent] = useState(true);
+
+    const editModeChange = () => {
+        setEditMode(!editMode);
+    };
+
+    const nameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.target.value);
+    };
+
+    const studentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setIsStudent(event.target.checked);
+    };
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            {editMode ? (
+                <div>
+                    <input type="text" value={userName} onChange={nameChange} />
+                    <Form.Check
+                        type="checkbox"
+                        id="is-check-box"
+                        label="Student"
+                        checked={isStudent}
+                        onChange={studentChange}
+                        color="primary"
+                    />
+                </div>
+            ) : (
+                <div>
+                    {userName} is {isStudent ? "a student" : "not a student"}{" "}
+                </div>
+            )}
+            <Form.Check
+                type="switch"
+                id="is-edit-mode"
+                label="Edit Mode"
+                checked={editMode}
+                onChange={editModeChange}
+                color="primary"
+            />
         </div>
     );
 }
